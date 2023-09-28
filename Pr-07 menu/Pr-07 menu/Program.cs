d7afefd3359c7fe5c0ac2bb12ed8b60f18e2c571
TheBadAngel <144236013+TheBadAngel@users.noreply.github.com>
2023-09-28 16:36:23 +0200

    Add files via upload

---
+++ Pr-07 menu/Pr-07 menu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pr_07_menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
