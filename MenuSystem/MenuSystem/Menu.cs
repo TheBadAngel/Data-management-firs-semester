f044beafc32cb4ee24fdcad323200d435c5b752b
TheBadAngel <144236013+TheBadAngel@users.noreply.github.com>
2023-09-28 16:41:36 +0200

    Add files via upload

---
+++ MenuSystem/MenuSystem/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuSystem
+{
+    internal class Menu
+    {
+        public string Title = "Min fantastiske menu";
+        private menuItem[] MenuItems;
+        private int itemCount = 0;
+       
+        public void Show()
+        {
+
+            Console.WriteLine(Title + "\n");
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                Console.WriteLine(MenuItems[i].Title);
+            }
+
+            Console.WriteLine("\n(Tryk menupunkt eller 0 for at afslutte)");
+        }
+
+
+    }
+}
