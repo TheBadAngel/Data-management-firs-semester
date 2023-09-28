d4ea28c694a413d178d64970818af160c7e811e6
TheBadAngel <144236013+TheBadAngel@users.noreply.github.com>
2023-09-28 14:41:32 +0200

    Add files via upload

---
+++ Datahåndtering/Datahåndtering/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Claims;
+
+namespace Datahåndtering
+{
+    internal class Program
+    {
+        class person
+        {
+            public int age; //int er heltal
+            public string firstname; //string er tekst
+            public string lastname; //string er tekst
+            public bool glasses; // bool er true eller false
+            public float weight; // Float er decimaltal
+            private int cprnumber;
+
+            // public kan ander se og private er skjult
+
+
+        }
+
+
+        class car
+        {
+            public string wheels;
+            public int doors;
+            public string engine;
+            private string platenumber;
+            public bool windows;
+
+        }
+
+
+
+        class product
+        {
+            public string name;
+
+
+
+        }
+
+
+
+
+
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+    }
+}
+}
