2608e17ad2ca2d3f088b0b1ccfa130547ede3eec
TheBadAngel <144236013+TheBadAngel@users.noreply.github.com>
2023-10-03 18:48:30 +0200

    Add files via upload

---
+++ CSV-test/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_test
+{
+    public class Employee
+    {
+
+        public int Id;
+        public string FirstName;
+        public string LastName;
+
+
+        public Employee(int Id, string FirstName, string LastName)
+        {
+
+            this.Id = Id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+
+        }
+
+
+
+    }
+
+
+}
+
---
+++ CSV-test/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CSV_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indput Id, Fornavn og Efternavn.");
+            Employee[] Medarbejder =
+            {
+             new Employee(
+                 int.Parse(Console.ReadLine()),//ID number
+                 Console.ReadLine(), // First Name
+                 Console.ReadLine()) //Last Name
+            };
+
+            String file = "medarbejder.csv";
+            String separator = ",";
+            StringBuilder medarbejder = new StringBuilder();
+            String[] headings = { "Medarbejder Id", "First Name", "Last Name" };
+            medarbejder.Append(string.Join(separator, headings));
+
+
+            foreach (Employee employee in Medarbejder)
+            {
+                String[] newLine = { employee.Id.ToString(), employee.FirstName, employee.LastName };
+                medarbejder.Append(string.Join(separator, newLine));
+            }
+
+            try
+            {
+                File.AppendAllText(file, medarbejder.ToString());
+
+            }
+            catch
+            {
+                Console.WriteLine("Data could not be written to the CSV file.");
+                return;
+            }
+
+            Console.WriteLine("The data has been successfully saved to the CSV file.");
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
